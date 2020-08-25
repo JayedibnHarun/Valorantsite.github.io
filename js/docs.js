c770c2623930a509513f2b7764dca69bef0710eb
JayedibnHarun <70161942+JayedibnHarun@users.noreply.github.com>
2020-08-25 22:06:06 +0600

    My gaming Site

---
+++ js/docs.js
@@ -0,0 +1,26 @@
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Gaming',     4],
+          ['Eat',      2],
+          ['Web Design',  3],
+          ['Watch TV', 2],
+          ['Sleep',    8],
+          ['Workout', 1]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+
+      
+      
